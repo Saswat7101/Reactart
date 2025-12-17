984d33d1925d282d7edb5e73ca3c6197b1cb38b9
Saswat <mohantysaswat154@gmail.com>
2025-12-17 17:07:54 +0530

    setting up tailwind v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ['"Pacifico"', "cursive"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
